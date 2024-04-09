fa1dac16dd16426b50acf5544190134ada8ba833
CER-UFBA <154695081+CER-UFBA@users.noreply.github.com>
2024-04-09 19:46:26 -0300

    R script - Bayesian Unit control Charts

--- aplication/ap_unit-bayesian.R
+++ aplication/ap_unit-bayesian.R
@@ -1,4 +1,6 @@
 source("bayesian function chart.R")
-s.min = read.csv("https://raw.githubusercontent.com/Andersonof30/chart/main/serie%20da%20minima%20da%20UR.csv", sep = " ")
+
+s.min = read.csv("https://raw.githubusercontent.com/Andersonof30/chart/main/min_UR.csv", sep = " ")
+
 plot.control(s.min$minima, type = 'unit', fase1 = .8, xlab = '', ylab = 'Umidade Relativa do Ar', 
              tit = ' ', size =  20, iter = 1000)
