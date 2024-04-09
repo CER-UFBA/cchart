de31150701024efcc9562cec6e50ea202ce1e6bc
CER-UFBA <154695081+CER-UFBA@users.noreply.github.com>
2024-04-09 19:50:38 -0300

    Update ap_unit-bayesian.R

--- aplication/ap_unit-bayesian.R
+++ aplication/ap_unit-bayesian.R
@@ -1,6 +1,6 @@
-source("bayesian function chart.R")
+source("bayesian_function_chart.R")
 
-s.min = read.csv("https://raw.githubusercontent.com/Andersonof30/chart/main/min_UR.csv", sep = " ")
+s.min = read.csv("https://raw.githubusercontent.com/CER-UFBA/cchart/main/data/min_UR.csv", sep = " ")
 
 plot.control(s.min$minima, type = 'unit', fase1 = .8, xlab = '', ylab = 'Umidade Relativa do Ar', 
              tit = ' ', size =  20, iter = 1000)
