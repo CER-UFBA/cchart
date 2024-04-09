7ec08ff2465235251909bb246a6d6247ba18d20a
CER-UFBA <154695081+CER-UFBA@users.noreply.github.com>
2024-04-09 19:47:54 -0300

    R script - Classic Unit control Charts

---
+++ aplication/ap_unit-classic.R
@@ -0,0 +1,6 @@
+source("classical_function_chart.R")
+
+s.min = read.csv("https://raw.githubusercontent.com/Andersonof30/chart/main/min_UR.csv", sep = " ")
+
+plot.control(s.min$minima, type = 'unit', fase1 = .8, xlab = '', ylab = 'Umidade Relativa do Ar', 
+             tit = ' ', size =  20, iter = 1000)
